c26ab88a48f7698433806a2e29928d14eeeb40e9
jwar5 <jesslw4265@gmail.com>
2019-12-04 02:57:14 -0500

    part9

--- config.py
+++ config.py
@@ -12,3 +12,4 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['jwarre24@kent.edu']
+    POSTS_PER_PAGE = 25
